6714e25cc9bc2e3de37a6fc79e636d65b29313aa
Lance Anderson <lance.a.585@gmail.com>
2023-03-27 20:49:42 -0700

    Edit - working on tooltip

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // define svg width, weight, margins
 var margin = {top: 20, right: 10, bottom: 40, left: 100},
-    width = 1200 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    width = 1150 - margin.left - margin.right,
+    height = 650 - margin.top - margin.bottom;
 
 // The svg
-var svg = d3.select("svg")
+var svg = d3.select("#holder")
+            .append("svg")
+            .attr("id", "map")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform",
                       "translate(" + margin.left + "," + margin.top + ")");
+
+  // create tooltip div in #holder
+  const tooltip = d3.select("#holder")
+                  .append("div")
+                  .attr("id", "tooltip")
+
+
+
 
 var projection = d3.geoMercator()
             .scale(70)
@@ -19,10 +29,12 @@ var projection = d3.geoMercator()
   // define array for promises
   var promises = []
 
+  // push json to promises array
   promises.push(d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"))
   promises.push(d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"))
+  
+  // start .then function
   myDataPromises = Promise.all(promises).then(function(my_data) {
-
 
   // find min and max of bachelorsOrHigher
   const degreeArr = [];
@@ -30,37 +42,60 @@ var projection = d3.geoMercator()
     degreeArr.push(obj.bachelorsOrHigher)
   }
   const degreeExt = d3.extent(degreeArr)
-  // console.log(degreeExt)
-
+  
   // create colorScale
   const colorScale = d3.scaleLinear()
                     .range(["hsl(208, 48%, 80%)", "hsl(208, 48%, 20%)"])
                     .domain(degreeExt)
 
-
   // create topo for data in svg
   var topo = my_data[0]
+
   // create path for svg path elements
   var path = d3.geoPath();
 
+  // create paths to draw map
+  svg.append("g")
+      .selectAll("path")
+      .data(topojson.feature(topo, topo.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", "county")
+      .attr("stroke", "white")
+      .attr("fill", (d => {
+        let county = my_data[1].filter(obj => obj.fips === d.id)
+        // console.log(county[0].bachelorsOrHigher)
+        if (county[0]) {
+          return colorScale(county[0].bachelorsOrHigher)
+        } else {return colorScale(0)}
+        }))
+    // .attr("data-fips", (d => {
+    //   let fips = my_data[1].filter(obj => obj.fips === d.id)
+    //   if (fips[0]) {
+    //     // console.log(fips[0].fips)
+    //     return fips[0].fips
+    //   } else {return 0}
+    //   }))
+    // .attr("data-education", (d => {
+    //   let ed = my_data[1].filter(obj => obj.fips === d.id)
+    //   if (ed[0]) {
+    //     // console.log(ed[0].bachelorsOrHigher)
+    //     return ed[0].bachelorsOrHigher
+    //   } else {return 0}
+    //   }))
+    .on("mouseover", function(event, d) {
+    // TODO:  fix tooltip html - use example
+      tooltip.html("hi")
+          .style("display", "block")
+          .style("left", event.pageX + 20 + "px")
+          .style("top", event.pageY - 80 + "px")
+          .style("background-color", "lightgray")
+    })
 
-svg.append("g")
-.selectAll("path")
-.data(topojson.feature(topo, topo.objects.counties).features)
-    .enter()
-    .append("path")
-  .attr("d", path)
-  .attr("fill", "steelblue")
-  .attr("stroke", "white")
-  .attr("fill", (d => {
-    let county = my_data[1].filter(obj => obj.fips === d.id)
-    // console.log(county[0].bachelorsOrHigher)
-    if (county[0]) {
-      return colorScale(county[0].bachelorsOrHigher)
-     } else {return colorScale(0)}
+    //   })
+    //   .on("mouseout", function() {
+    //     return tooltip.style("display", "none")
+    //   })
 
-  }))
-
-
-  });
-  
+      })
